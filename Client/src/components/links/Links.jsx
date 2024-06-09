b58a4d2160f91f2b3c464b59fc6166a5786a9f3b
umerxgit2004 <umer.workplace174@gmail.com>
2024-06-09 12:43:29 +0500

    fixed the staggerDirection

--- Client/src/components/links/Links.jsx
+++ Client/src/components/links/Links.jsx
@@ -1,6 +1,30 @@
+import {motion} from "framer-motion"
 
+const variants = {
+  open :{
+    transition : {
+      staggerChildren : 0.5
+    }
+  },
+  close :{
+    transition : {
+      staggerChildren : 0.05,
+      staggerDirection : -1
+    }
+  }
+}
 
-const Links = () => {
+const itemVariants = {
+  open : {
+    y:0,
+    opacity : 1
+  },
+  close :{
+    y: 50,
+    opacity : 0
+  }
+}
+const Links = ({isOpen}) => {
   const items = [
     "Homepage",
     "Services",
@@ -9,13 +33,16 @@ const Links = () => {
     "About"
   ]
   return (
-    <div className="links">
-      {items.map (item => (
-        <a  href = {`#${item}`} key = {item}>
+    <motion.div className="links"  variants={variants} animate={isOpen? "open" : "close"}>
+      {items.map ((item) => (
+        <motion.a  href = {`#${item}`} key = {item}
+         variants ={itemVariants}
+         whileHover = {{scale:1.1}}
+         whileTap={{scale:0.95}}>
           {item}
-        </a>
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
   )
 }
 
